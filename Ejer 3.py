eba94cbc5d3b97245798f66b519ed80638655f7a
Omarjr33 <omarjrcardona33@gmail.com>
2024-10-16 07:09:46 -0500

    taller

---
+++ Ejer 3.py
@@ -0,0 +1,16 @@
+"""Crea un programa que solicite al
+usuario un número entero positivo y
+luego imprima los números desde ese
+número hasta 1 utilizando un
+buclewhile."""
+
+def numeroEntero (numero:int):
+    while (numero > 0):
+        print (f'Del {numero} al {numero} ')
+        numero -=1
+        return
+
+if (__name__=='__main__'):
+   if numeroEntero > 0 :
+       pass
+
---
+++ Ejer 5.py
@@ -0,0 +1,15 @@
+"""Crea un programa que pida al usuario ingresar el nombre de un país y luego
+determine en qué continente se encuentra.Utiliza estructuras condicionales para
+asociar cada país con su respectivo continente y muestra un mensaje con el
+continente correspondiente."""
+
+def continentes (conti):
+    asia = ("Japón", "China","Mongolia", "Corea del Norte", "Corea del Sur", "Filipinas", "Vietnam")
+    africa = ("Angola", "Luanda", "Argelia", "Argel", "Benín", "Porto Novo", "Botsuana", "Gaborone")
+    américaDelNorte = ("Antigua y Barbuda", "Bahamas", "Barbados","Canadá","Costa Rica","Cuba","Dominica","Granda")
+    américaDelSur = ("Argentina","Bolivia","Brasil","Chile","Colombia","Ecuador","Guyana","Perú")
+    antártida = ("Argentina", "Australia", "Chile","Nueva Zelanda")
+    europa = ("Bélgica", "Bulgaria", "Chequia", "Dinamarca", "Alemania", "Estonia", "Irlanda", "Grecia", "España", "Francia")
+    oseania = ("Estados Unidos","Reino Unido","Francia","Nueva Zelanda","Australia")
+    
+
